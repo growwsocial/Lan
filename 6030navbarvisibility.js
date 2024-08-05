1921eb313158dc1c082c02bc8eaf84598f44bc39
growwsocial <growwsocialshop@gmail.com>
2024-08-05 16:43:33 +0530

    Update 6030navbarvisibility.js

--- 6030navbarvisibility.js
+++ 6030navbarvisibility.js
@@ -1,1 +1,13 @@
-
+$(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 10) {
+        $(".navbar").addClass("shrink");
+      } else {
+        $(".navbar").removeClass("shrink");
+      }
+      if (scroll >= 300) {
+        $(".navbar").addClass("fixed-top");
+      } else {
+        $(".navbar").removeClass("fixed-top");
+      }
+    });
